9e84a47a45b8ef54ed95689bb08d6123dac1d63a
Amrit <33424649+Spaceface16518@users.noreply.github.com>
2018-01-30 14:49:07 -0600

    NodeJS version
    This is a nodejs version of the python script that should essentially do the same thing. See issue #1.

---
+++ app.js
@@ -0,0 +1,21 @@
+const argsv = require('yargs').parse()
+  .command('credentials <USER> <PASS> <CLIENT_ID> <CLIENT_SECRET>', 'Enter your Reddit bot credentials', (argsv) => {
+    argsv.positional('USER', {
+    describe: 'Reddit username',
+    type: 'string',
+    default: 'Spaceface16518'
+  }).positional('PASS', {
+    describe: 'Reddit password',
+    tyoe: 'string',
+    default: ''
+  }).positional('CLIENT_ID', {
+      describe: 'Reddit app client ID',
+      type: 'string',
+      default: ''
+}).postional('CLIENT_SECRET', {
+      describe: 'Reddit app client secret key',
+      type: 'strin',
+      default: ''
+  }
+  .help()
+  .argv
